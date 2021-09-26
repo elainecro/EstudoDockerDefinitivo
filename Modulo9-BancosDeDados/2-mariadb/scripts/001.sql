7fa36be7d14c9d1001dea3ddbfe47ef58ec7098d
Elaine Oliveira <elaine.oliveira@itixti.com.br>
2021-09-26 16:18:39 -0300

    docker-compose para mariadb

---
+++ Modulo9-BancosDeDados/2-mariadb/scripts/001.sql
@@ -0,0 +1,9 @@
+CREATE TABLE nanedb.pessoamariadb (
+    idpessoa int not null,
+    nome varchar(45) null,
+    sobrenome varchar(45) null,
+    primary key (idpessoa)
+);
+
+INSERT INTO pessoa VALUES (1, 'Elaine', 'Oliveira');
+INSERT INTO pessoa VALUES (2, 'Maria', 'Rocha');
