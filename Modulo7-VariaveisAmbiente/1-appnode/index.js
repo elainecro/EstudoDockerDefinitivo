5bd231043294bc0ff520b7d334fcc9326f1cfbff
Elaine Oliveira <elaine.oliveira@itixti.com.br>
2021-09-25 22:59:59 -0300

    Estudo Docker Definitivo - Imagens, containers, volumes, variaveis de ambiente

---
+++ Modulo7-VariaveisAmbiente/1-appnode/index.js
@@ -0,0 +1,6 @@
+var prefix = process.env.prefix || " - ";
+var appUrl = process.env.prefix + ".app.com";
+
+console.log("-----------------------");
+console.log(appUrl);
+console.log("-----------------------");
